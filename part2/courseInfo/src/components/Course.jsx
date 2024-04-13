80f75c13721c33d564ac939eaac8309086264d74
Parin Parikh <59714790+parin-parikh@users.noreply.github.com>
2024-04-13 14:55:36 -0400

    Completed Exercise 2.11

--- part2/courseInfo/src/components/Course.jsx
+++ part2/courseInfo/src/components/Course.jsx
@@ -1,21 +1,21 @@
 const Course = (props) => {
   return (
     <div>
-        {
-          props.course.map(course => (
-            <div key={course.id}>
-              <h2>{course.name}</h2>
-              <div>
-                {course.parts.map(part => (
-                  <p key={part.id}>
-                    {part.name} {part.exercises}
-                  </p>
-                ))}            
-              </div>
-              <h4>total of {course.parts.reduce((acc, part) => acc + part.exercises, 0)} exercises</h4>
+      {
+        props.course.map(course => (
+          <div key={course.id}>
+            <h2>{course.name}</h2>
+            <div>
+              {course.parts.map(part => (
+                <p key={part.id}>
+                  {part.name} {part.exercises}
+                </p>
+              ))}            
             </div>
-          ))
-        }
+            <h4>total of {course.parts.reduce((acc, part) => acc + part.exercises, 0)} exercises</h4>
+          </div>
+        ))
+      }
     </div>
   )
 }
