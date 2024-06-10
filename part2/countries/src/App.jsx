9c565982fa02cab710b5c86d2f820aa11c202881
Parin Parikh <59714790+parin-parikh@users.noreply.github.com>
2024-06-10 16:06:24 -0400

    commit

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,12 +1,41 @@
-import { useEffect, useState } from 'react'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [country, setCountry] = useState([])
+  const [error, setError ] = useState('')
+
+  useEffect(() => {
+    handleSearch(searchInput);
+  }, [searchInput]);
+
+  const handleSearch = (input) => {
+    fetch(`https://restcountries.com/v3.1/name/${input}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        if (data.status === 404) {
+          setError('No country found. Please try again.');
+        } else if (data.length > 10) {
+          setError('Too many countries found. Please make your query more specific.');
+        } else {
+          setCountry(data);
+          setError('');
+        }
+      })
+  }
 
   return (
     <>
       <div>
-        find countries <input type="text" value={country} />
+        find countries <input type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
       </div>
+
+      <pre>
+        {JSON.stringify(country, null, 2)}
+      </pre>
     </>
   )
 }
